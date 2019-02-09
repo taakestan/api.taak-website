52eff0c2bf3a35c2a84ce41951fca446db6228dd
hasanteymoori <hasan.teymoori1372@gmail.com>
2019-02-09 15:34:59 +0330

    app init

---
+++ tests/Feature/Models/WebinarTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\Unit\Models;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class WebinarTest extends TestCase {
+
+    use RefreshDatabase;
+
+
+}
