6efb967b8702a1203671d8a50d9e570ebcc292ec
hasanteymoori <hasan.teymoori1372@gmail.com>
2019-02-09 16:26:37 +0330

    create factory and migration and fill some booting of webinar model

--- app/Models/Webinar.php
+++ app/Models/Webinar.php
@@ -12,5 +12,47 @@ use Illuminate\Database\Eloquent\Model;
  */
 class Webinar extends Model
 {
-    //
+    /**
+     * The attributes that aren't mass assignable.
+     *
+     * @var array
+     */
+    protected $guarded = [
+        'id', 'slug'
+    ];
+
+    #-------------------------------------##   <editor-fold desc="Booting">   ##----------------------------------------------------#
+
+    /**
+     * The "booting" method of the model.
+     *
+     * @return void
+     */
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($product) {
+            $product->slug = $product->en_title;
+        });
+
+        static::updating(function ($product) {
+            $product->slug = $product->en_title;
+        });
+    }
+
+    # </editor-fold>
+
+    #-------------------------------------##   <editor-fold desc="The Mutator">   ##----------------------------------------------------#
+
+    /**
+     * create slug from the en_title of webinar
+     *
+     * @param $en_title
+     */
+    public function setSlugAttribute($en_title)
+    {
+        $this->attributes['slug'] = str_slug($en_title);
+    }
+    # </editor-fold>
 }
--- database/migrations/2019_02_09_121902_create_webinars_table.php
+++ database/migrations/2019_02_09_121902_create_webinars_table.php
@@ -15,6 +15,11 @@ class CreateWebinarsTable extends Migration
     {
         Schema::create('webinars', function (Blueprint $table) {
             $table->increments('id');
+
+            $table->string('title');
+            $table->string('en_title');
+            $table->string('slug')->unique()->index();
+
             $table->timestamps();
         });
     }
