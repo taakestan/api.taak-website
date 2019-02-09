5d7dfc607b6f65c5dc26c8c482883637bb9954e4
hasanteymoori <hasan.teymoori1372@gmail.com>
2019-02-09 15:58:12 +0330

    add helpers and webinar model testing

--- tests/Feature/Models/WebinarTest.php
+++ tests/Feature/Models/WebinarTest.php
@@ -2,12 +2,74 @@
 
 namespace Tests\Unit\Models;
 
+use App\Models\Webinar;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class WebinarTest extends TestCase {
 
-    use RefreshDatabase;
+    use RefreshDatabase, WithFaker;
 
+    #-------------------------------------##   <editor-fold desc="The setUp">   ##----------------------------------------------------#
+
+    /**
+     * @var Webinar $webinar
+     */
+    protected $webinar;
+
+    /**
+     * Setup the test environment.
+     *
+     * @return void
+     */
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->webinar = create(Webinar::class);
+    }
+
+    # </editor-fold>
+
+    #-------------------------------------##   <editor-fold desc="The Booting">   ##----------------------------------------------------#
+
+    /** @test */
+    public function it_attach_slug_to_model_object_when_creating_webinar()
+    {
+        $this->assertArrayHasKey(
+            'slug', $this->webinar->toArray()
+        );
+    }
+
+    /** @test */
+    public function it_update_slug_to_new_en_title_when_updating_en_title()
+    {
+        $this->webinar->update([
+            'en_title' => $en_title = 'the new english title'
+        ]);
+
+        $this->assertEquals(
+            $this->webinar->en_title, $en_title
+        );
+    }
+
+    # </editor-fold>
+
+    #-------------------------------------##   <editor-fold desc="The Mutator">   ##----------------------------------------------------#
+
+    /** @test */
+    public function it_should_create_slug_from_the_en_title_of_webinar()
+    {
+        $webinar = create(Webinar::class, [
+            'en_title' => $en_title = $this->faker->sentence
+
+        ]);
+
+        $this->assertEquals(
+            $webinar->slug, str_slug($en_title)
+        );
+    }
+
+    # </editor-fold>
 
 }
---
+++ tests/Tools/helpers.php
@@ -0,0 +1,46 @@
+<?php
+
+if (!function_exists('create')) {
+    /**
+     * wrapper for factory()->create()
+     *
+     * @param string $class
+     * @param array $attribute
+     * @param null $times
+     * @return \Illuminate\Database\Eloquent\Model
+     */
+    function create(string $class, array $attribute = [], $times = null)
+    {
+        return factory($class, $times)->create($attribute);
+    }
+}
+
+if (!function_exists('make')) {
+    /**
+     * wrapper for factory()->make()
+     *
+     * @param string $class
+     * @param array $attribute
+     * @param null $times
+     * @return \Illuminate\Database\Eloquent\Model
+     */
+    function make(string $class, array $attribute = [], $times = null)
+    {
+        return factory($class, $times)->make($attribute);
+    }
+}
+
+if (!function_exists('raw')) {
+    /**
+     * wrapper for factory()->raw()
+     *
+     * @param string $class
+     * @param array $attribute
+     * @param null $times
+     * @return array
+     */
+    function raw(string $class, array $attribute = [], $times = null)
+    {
+        return factory($class, $times)->raw($attribute);
+    }
+}
