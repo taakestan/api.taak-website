d2eed6da722e93f832f904afb5353b075a806c8c
hasanteymoori <hasan.teymoori1372@gmail.com>
2019-02-09 15:58:30 +0330

    create model and migration for webinar

---
+++ app/Models/Webinar.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @property integer id
+ * @property string title
+ * @property string en_title
+ * @property string slug
+ */
+class Webinar extends Model
+{
+    //
+}
